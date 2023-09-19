8c7454aa0bf9c015753556c7823dbcffd6308219
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-09-19 12:08:10 +0300

    wedding start with......

--- src/main/java/com/company/Wedding.java
+++ src/main/java/com/company/Wedding.java
@@ -15,28 +15,11 @@ public class Wedding extends Event {
 
     @Override
     public void start()
-    {package com.company;
+    {
+        super.start();
 
-        public class Wedding extends Event {
-            private String weddingCake;
-
-
-            public Wedding(Organiser chefOrganiser, int numberOfGuests, String weddingCake) {
-                super(chefOrganiser, numberOfGuests);
-                this.weddingCake = weddingCake;
-            }
-
-            public String getWeddingCake() {
-                return weddingCake;
-            }
-
-            @Override
-            public void start()
-            {
-                super.start();
-
-                if(super.isStarted()){
-                    System.out.println("Wedding started with " + weddingCake + "!");
-                }
-            }
+        if(super.isStarted()){
+            System.out.println("Wedding started with " + weddingCake + "!");
         }
+    }
+}
