d9e41eb131222e105d701f5fd17df20d0dc4cb40
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-09-19 11:44:53 +0300

    guests can ask for a goodie bag, but only one goodie bag per guest.

---
+++ src/main/java/com/company/Conference.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Conference extends Event {
+    private boolean goodieBagsDistributed;
+    private int numberOfGoodieBags;
+
+    public Conference(Organiser chefOrganiser, int numberOfGuests, int numberOfGoodieBags) {
+        super(chefOrganiser, numberOfGuests);
+        this.numberOfGoodieBags = numberOfGoodieBags;
+        this.goodieBagsDistributed = false;
+    }
+
+    public void askForGoodieBags(){
+        if(!goodieBagsDistributed) {
+            numberOfGoodieBags -= getNumberOfGuests();
+
+            if (numberOfGoodieBags < 0) {
+                numberOfGoodieBags = 0;
+                System.out.println("No goodie bags left!");
+            }
+            goodieBagsDistributed = true;
+        }
+        else {
+            System.out.println("Goodie bags already distributed!");
+        }
+    }
+}
