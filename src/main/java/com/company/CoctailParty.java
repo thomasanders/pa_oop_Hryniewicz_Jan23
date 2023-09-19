e6f3912ae615835553dc3f3aca14edeea9259627
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-09-19 11:47:18 +0300

    request, cocktail

---
+++ src/main/java/com/company/CoctailParty.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class CocktailParty extends Event {
+    private int numberOfCocktails;
+
+    public CocktailParty(Organiser chefOrganiser, int numberOfGuests, int numberOfCocktails) {
+        super(chefOrganiser,numberOfGuests);
+        this.numberOfCocktails = numberOfCocktails;
+    }
+
+    public void requestCoktails()
+    {
+        numberOfCocktails -= getNumberOfGuests();
+
+        if(numberOfCocktails <= 0){
+            numberOfCocktails = 0;
+            System.out.println("No cocktails left!");
+        }
+    }
+}
